7216bae7a37e25592babbaa1815841aa1473be63
Paul Abrams <john.paul.abrams@gmail.com>
2022-05-04 20:01:05 -0400

    feat: singl-char input  enter not required

--- go.py
+++ go.py
@@ -4,15 +4,18 @@ def single_input(prompt, colPos=1, rowPos=-1):
     """
         Reads a single char beside specified prompt, which is shown at the specified position.
         Default position is bottom row of current term display, first column.
-        TODO: Don't require [enter] after single-char input.
     """
     if rowPos  == -1:
         rowPos = term.state(term.TK_HEIGHT) - 1
     term.printf(colPos, rowPos, prompt)
-    inlineKey = ''
-    key = term.read_str(len(prompt) + colPos + 1, rowPos, inlineKey, 1)
-    return key[1]
+    term.refresh()
+    key = term.read()
+    if key == term.TK_CLOSE:
+        exit
+    else:
+        return chr(term.state(term.TK_WCHAR))
 term.open()
+term.refresh()
 term.printf(1, 1, 'Hello, world! Use q to quit.')
 key = single_input('your choice: ')
 if key != 'q': 
